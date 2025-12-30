c0c609e48049311f2b3c22ec43967385dbf3a270
li6dbc <li6dbc@ME-6D59CY3>
2025-12-30 10:45:30 -0500

    Ajoutez les fichiers .gitignore et .gitattributes.

---
+++ Gestionnaire_de_contacts/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Gestionnaire_de_contacts
+{
+    class Contact
+    {
+        private string prenom;
+        private string nom;
+        private string numero;
+
+        public string getPrenom()
+        {
+            return prenom;
+        }
+        public string getNom()
+        {
+            return nom;
+        }
+
+        public string getNumero()
+        {
+            return numero;
+        }
+
+        public void setPrenom(string prenom_to_set)
+        {
+            prenom = prenom_to_set;
+        }
+
+        public void setNom(string nom_to_set)
+        {
+            nom = nom_to_set;
+        }
+
+        public void setNumero(string numero_to_set)
+        {       
+            numero = numero_to_set;
+
+        }
+
+        public bool isPrenomValid(string prenom_to_set)
+        {
+            bool valid = false;
+            if (prenom_to_set.Length > 0){
+                valid = true;
+            }
+            return valid;
+        }
+        public bool isNomValid(string nom_to_set)
+        {
+            bool valid = false;
+            if (nom_to_set.Length > 0)
+            {
+                valid = true;
+            }
+            return valid;
+        }
+        public bool isNumeroValid(string numero_to_set)
+        {
+            bool valid = false;
+            if (Regex.IsMatch(numero_to_set, @"^\+?\d{1,3}?[-.\s]?\(?\d{1,4}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"))
+            {
+                valid = true;
+            }
+            return valid;
+        }
+
+        public string getAll()
+        {
+            return getPrenom() + " " + getNom() + " " + getNumero();
+        }
+    }
+}
---
+++ Gestionnaire_de_contacts/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace Gestionnaire_de_contacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choix = "0";
+            List<Contact> liste_contact = new();
+            string chemin = Path.Combine(Environment.CurrentDirectory, "contacts.txt");
+            string contenu = File.ReadAllText(chemin);
+            while (choix != "4")
+            {
+                try
+                {
+                    Console.WriteLine("Bienvenue dans le gestionnaire de contacts");
+                    Console.WriteLine("1 - Voir les contacts");
+                    Console.WriteLine("2 - Ajouter un contact");
+                    Console.WriteLine("3 - Supprimer un contact");
+                    Console.WriteLine("4 - Quitter");
+                    choix = Console.ReadLine();
+                    Console.Clear();
+                    switch (choix)
+                    {
+                        case "1":
+                            contenu = File.ReadAllText(chemin);
+                            if (contenu.Length > 0)
+                            {
+                                Console.WriteLine(contenu);
+                            } else
+                            {
+                                Console.WriteLine("Il n'y a pas de contacts à afficher.");
+                            }
+                            break;
+                        case "2":
+                            Contact contact = new Contact();
+                            string prenom, nom, numero;
+                            Console.WriteLine("Veuillez entrer un prenom : ");
+                            prenom = Console.ReadLine();
+                            while (!contact.isPrenomValid(prenom))
+                            {
+                                Console.WriteLine("Erreur - Veuillez entrer un prenom valide : ");
+                                prenom = Console.ReadLine();
+                            }
+                            contact.setPrenom(prenom);
+                            Console.WriteLine("Veuillez entrer un nom : ");
+                            nom = Console.ReadLine();
+                            while (!contact.isNomValid(nom))
+                            {
+                                Console.WriteLine("Erreur - Veuillez entrer un nom valide : ");
+                                nom = Console.ReadLine();
+                            }
+                            contact.setNom(nom);
+                            Console.WriteLine("Veuillez entrer un numéro de téléphone : ");
+                            numero = Console.ReadLine();
+                            while (!contact.isNumeroValid(numero))
+                            {
+                                Console.WriteLine("Erreur - Veuillez entrer un numéro de téléphone valide : ");
+                                numero = Console.ReadLine();
+                            }
+                            contact.setNumero(numero);
+                            Console.WriteLine("Contact Ajouté");
+                            liste_contact.Add(contact);
+                            foreach (var cont in liste_contact)
+                            {
+                                File.AppendAllText(chemin, (File.ReadAllLines(chemin).Length + 1) + " " + cont.getAll() + "\n");
+                            }
+                            liste_contact.Clear();
+                            break;
+                        case "3":
+                            int numero_a_supprimer = 0;
+                            Console.WriteLine(contenu);
+                            var lignes = File.ReadAllLines(chemin).ToList();
+                            if (lignes.Count > 0)
+                            {
+                                bool succes = false;
+                                string s = "";
+                                do
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("Quel contact souhaitez-vous supprimer ? : ");
+                                        s = Console.ReadLine();
+                                        numero_a_supprimer = int.Parse(s);
+
+                                        while (numero_a_supprimer > lignes.Count)
+                                        {
+                                            Console.WriteLine("Erreur - Quel contact souhaitez-vous supprimer ? : ");
+                                            s = Console.ReadLine();
+                                            numero_a_supprimer = int.Parse(s);
+                                        }
+                                        succes = true;
+                                    }
+                                    catch (Exception err)
+                                    {
+                                        Console.WriteLine("Erreur - Veuillez entrer un nombre");
+                                    }
+
+                                } while (!succes);
+                                lignes.RemoveAt(numero_a_supprimer - 1);
+                                File.WriteAllLines(chemin, lignes);
+                                string[] arr_lignes = File.ReadAllLines(chemin);
+                                for (int i = 0; i < arr_lignes.Length; i++)
+                                {
+                                    arr_lignes[i] = arr_lignes[i].Substring(1);
+                                    arr_lignes[i] = (i + 1).ToString() + arr_lignes[i];
+                                }
+                                File.WriteAllLines(chemin, arr_lignes);
+                                Console.WriteLine("Ligne supprimée");
+                            } else
+                            {
+                                Console.WriteLine("Il n'y a pas de contacts à supprimer.");
+                            }
+                            break;
+
+                        case "4":
+                            Console.WriteLine("Merci d'avoir utilisé le Gestionnaire de contacts");
+                            break;
+                        default:
+                            Console.WriteLine("Veuillez choisir une option valide.");
+                            break;
+
+                    }
+
+                    Console.WriteLine("Appuyez sur une touche pour continuer...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                catch (Exception erreur)
+                {
+                    Console.WriteLine("Une Erreur est survenue.");
+                }
+            }
+        }
+
+    }
+}
